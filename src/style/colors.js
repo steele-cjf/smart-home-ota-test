7adf629936f5f7ffffa71088f10ff32362946423
xionghuixi <potter@deMacBook-Pro.local>
2020-08-03 14:59:29 +0800

    fix: color

--- src/style/colors.js
+++ src/style/colors.js
@@ -5,19 +5,19 @@ export const Theme = {
   tabIconDefault: '#C7C7C7',
   tabIconSelected: tintColor,
   primary: '#5C8BFF', // 主题色
-  secondary: '#262626', // 次要主题色
-  accent: '#4caf50', // 强调色
-  red: '#ff5722', // 红色，错误色
-  yellow: '#ffeb3b', // 黄色，警告色
-  grey: '#e3e3e3', // 银灰色
-  inverse: '#333333', // 反色
+  // secondary: '#262626', // 次要主题色
+  // accent: '#4caf50', // 强调色
+  // red: '#ff5722', // 红色，错误色
+  // yellow: '#ffeb3b', // 黄色，警告色
+  // grey: '#e3e3e3', // 银灰色
+  // inverse: '#333333', // 反色
   border: '#C7C7C7',
   background: '#fff', // 全局背景色
   cardBackground: '#F0F0F0', // 模块背景色
 
   textDefault: '#282828', // 默认文本
   textSecondary: '#7C7C7C', // 次要文本
-  textMuted: '#eee', // 禁用文本
+  // textMuted: '#eee', // 禁用文本
 
   textTitle: '#282828', // 标题文本
   textLink: '#5C8BFF', // 链接文本
