89faceb4953ea889a17924cd6b47f4dadd8837fa
luyiqiao <yiqiao.lu@things-matrix.com>
2020-08-06 19:21:58 +0800

    T2

--- src/page/Home/PassportVertify/component.js
+++ src/page/Home/PassportVertify/component.js
@@ -62,7 +62,7 @@ export default function PassportVertifyPage(props) {
             <ScrollView style={styles.scrollContainer}>
                 <Text style={styles.textTitle}>基本资料</Text>
                 <Form config={vertifyCn} class={styles.formBox} changeForm={changeForm}></Form>
-                <Text style={[styles.font18, styles.textTitle]}>照片上传</Text>
+                <Text style={styles.textTitle}>照片上传</Text>
                 <View style={styles.ImageUploadBox}>
                     <ImageUpload title='护照个人信息' setImageForm={(obj) => setImageForm(0, obj)} />
                     <ImageUpload title='护照入境信息' setImageForm={(obj) => setImageForm(1, obj)} />
@@ -78,21 +78,15 @@ export default function PassportVertifyPage(props) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        fontSize: 18
     },
     scrollContainer: {
-        // paddingTop: 30,
-        // paddingHorizontal: 20
         paddingTop: 16, 
         paddingHorizontal: 16, 
-        backgroundColor: 'white',
-    },
-    font18: {
-        fontSize: 18
+        backgroundColor: Theme.background,
     },
     textTitle: {
         fontSize: 16,
-        color: '#282828',
+        color: Theme.textDefault,
     },
     formBox: {
         paddingBottom: 16,
