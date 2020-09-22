944c49f8631d723cdf23bd576852576ded1f3d2e
vita <vita@vitadeMacBook-Pro.local>
2020-09-22 12:03:21 +0800

    fix: icon

--- src/common/Icomoon.js
+++ src/common/Icomoon.js
@@ -1,5 +1,5 @@
 import createIconSet from 'react-native-vector-icons/lib/create-icon-set';
-import glyphMap from '../assets/icomoon.json';
+import glyphMap from '../assets/fonts/iconfont.json';
 
 const iconSet = createIconSet(glyphMap, 'Icomoon', 'icomoon.ttf');
 
